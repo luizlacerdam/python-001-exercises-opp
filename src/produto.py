8b12cb8e0a1ae26305f743f99bb0ea734bf30ce7
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-19 14:45:41 -0400

    :sparkles: [feat] class-Produto

--- src/produto.py
+++ src/produto.py
@@ -2,19 +2,25 @@ class Produto:
     def __init__(
         self, nome: str, codigo: str, preco: float, quantidade: int
     ) -> None:
-        pass
+        self.nome = nome
+        self.codigo = codigo
+        self.preco = preco
+        self.quantidade = quantidade
 
     def get_preco(self) -> float:
-        pass
+        return self.preco
 
     def get_quantidade(self) -> int:
-        pass
+        return self.quantidade
 
     def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+        if novo_preco < 0:
+            raise ValueError("Preço não pode ser negativo")
+        else:
+            self.preco = novo_preco
 
     def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        self.quantidade += quantidade
 
     def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        self.quantidade -= quantidade
